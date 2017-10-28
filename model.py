36133b695b90519710177ecf2a2cedba654f186f
stereos <stereo1@gmail.com>
2017-10-28 11:39:19 +0200

    Added more images and nem model with cropping

--- model.py
+++ model.py
@@ -8,14 +8,24 @@ with open('data/driving_log.csv', 'r') as file:
 images=[]
 measurements=[]
 for line in lines:
-    filename = line[0].split('/')[-1]
-
-    current_path = 'data/IMG/' + filename
-    image=cv2.imread(current_path)
-
-    images.append(image)
-
-    measurements.append(float(line[3]))
+    #todo, make nicer
+    center_file = line[0].split('/')[-1]
+    left_file = line[1].split('/')[-1]
+    right_file = line[2].split('/')[-1]
+    center_path = 'data/IMG/' + center_file
+    left_path = 'data/IMG/' + left_file
+    right_path = 'data/IMG/' + right_file
+    center_image=cv2.imread(center_path)
+    left_image = cv2.imread(left_path)
+    right_image = cv2.imread(right_path)
+    steering=float(line[3])
+    correction = 0.2
+    images.append(center_image)
+    measurements.append(steering)
+    images.append(left_image)
+    measurements.append(steering+correction)
+    images.append(right_image)
+    measurements.append(steering-correction)
 
 #what about having np array to start with instead of converting?
 
@@ -28,7 +38,8 @@ from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Conv2D, MaxPooling2D
 
 model= Sequential()
-model.add(Lambda(lambda x: x/255.0 -0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/255.0 -0.5))#, input_shape=(160,320,3)))
 model.add(Conv2D(6,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Conv2D(6,5,5,activation="relu"))
@@ -38,6 +49,6 @@ model.add(Dense(120))
 model.add(Dense(84))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True,epochs=5)
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=5)
 
 model.save('model.h5')
