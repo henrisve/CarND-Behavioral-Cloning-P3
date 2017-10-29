5dfaedc14738807d87737b6643a55a71934e6b33
stereos <stereo1@gmail.com>
2017-10-29 17:17:41 +0100

    FIxed the generator, now it should work better

--- model.py
+++ model.py
@@ -3,67 +3,57 @@ import numpy as np
 import cv2
 import sklearn
 from sklearn.model_selection import train_test_split
+
+print("1")
+
+#use pandas instead?
 with open('data/driving_log.csv', 'r') as file:
     samples = [line for line in csv.reader(file, delimiter=',')][1:]
-
-
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 
-
-#todo, what aobut saving the data to file, and then...
+# todo, what aobut saving the data to file, and then...
 # use the generator to take lines from that file
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1:  # Loop forever so the generator never terminates
-        shuffle(samples)
+        sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset + batch_size]
 
             images = []
             angles = []
             for batch_sample in batch_samples:
-                center_file = line[0].split('/')[-1]
-                left_file = line[1].split('/')[-1]
-                right_file = line[2].split('/')[-1]
+                camera=np.random.randint(3)
+                flip=np.random.randint(2)
+                center_file = batch_sample[camera].split('/')[-1]
                 center_path = 'data/IMG/' + center_file
-                left_path = 'data/IMG/' + left_file
-                right_path = 'data/IMG/' + right_file
                 center_image = cv2.cvtColor(cv2.imread(center_path), cv2.COLOR_BGR2RGB)
-                left_image = cv2.cvtColor(cv2.imread(left_path), cv2.COLOR_BGR2RGB)
-                right_image = cv2.cvtColor(cv2.imread(right_path), cv2.COLOR_BGR2RGB)
-                steering = float(line[3])
-                correction = 0.2
+                correction = 0 if camera == 0 else (0.2 if camera == 1 else -0.2)
+                steering = float(batch_sample[3])+correction
+                if flip: # flip image and steering, so we have 50/50 for both left and right
+                    center_image=cv2.flip(center_image,1)
+                    steering=-steering
+
                 images.append(center_image)
                 angles.append(steering)
-                images.append(left_image)
-                angles.append(steering - correction)
-                #Flip images to get more data
-                #todo check this is correct
-                images.append(cv2.flip(center_image))
-                angles.append(steering + correction)
-                images.append(right_image)
-                angles.append(-steering)
-                images.append(cv2.flip(left_image))
-                angles.append(-steering - correction)
-                images.append(cv2.flip(right_image))
-                angles.append(-steering + correction)
 
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
+
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-#the training part, maybe split into two files?
+# the training part, maybe split into two files?
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda , Dropout
-from keras.layers import Conv2D, MaxPooling2D,Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Dropout
+from keras.layers import Conv2D, MaxPooling2D, Cropping2D
 
 model = Sequential()
 model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
@@ -84,6 +74,7 @@ model.add(Dropout(0.5, noise_shape=None, seed=None))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator,
+                    nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('model.h5')
